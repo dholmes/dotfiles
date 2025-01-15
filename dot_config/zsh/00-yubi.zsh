ec2d562d64a42427a27ec0890481df97234455dd
Dan Holmes <dholmes@articulate.com>
2025-01-15 17:35:29 -0600

    moves yubi config up a little in priority

---
+++ dot_config/zsh/00-yubi.zsh
@@ -0,0 +1,4 @@
+if which art >/dev/null; then
+  path+=/Applications/YubiKey\ Manager.app/Contents/MacOS
+  #export SSH_AUTH_SOCK="/Users/dholmes/Library/Application Support/art/yubikey.sock"
+fi
--- dot_config/zsh/yubi.zsh
+++
@@ -1,3 +0,0 @@
-
-path+=/Applications/YubiKey\ Manager.app/Contents/MacOS
-export SSH_AUTH_SOCK="$HOME/Library/Application Support/art/yubikey.sock"
